9640cc2d6df6d13fae31f0a0d177dae7496394d7
Erick Amorim <erick.ufpa@gmail.com>
2026-03-12 15:18:14 -0300

    update package

---
+++ R/hampel_filter.R
@@ -0,0 +1,15 @@
+#' @title Outlier data filter
+#' @description
+#' A short description...
+#'
+#' @param variable Quantitative variable
+#' @importFrom stats mad
+#' @import stats
+#' @return result
+
+hampel_filter <- function(variable){
+  limite_inf = stats::median(variable, na.rm = T) - 3 * stats::mad(variable, constant = 1, na.rm = T)
+  limite_sup = stats::median(variable, na.rm = T) + 3 * stats::mad(variable, constant = 1, na.rm = T)
+  result = (variable < limite_inf | variable > limite_sup)
+  return(result)
+}
