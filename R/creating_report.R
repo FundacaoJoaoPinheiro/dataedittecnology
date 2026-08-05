62ea0d2fabec36b044fb69c4e6eaa80f8d11e47e
Erick Amorim <erick.ufpa@gmail.com>
2026-08-05 10:00:44 -0300

    update package

--- R/creating_report.R
+++ R/creating_report.R
@@ -4,17 +4,12 @@
 #'
 #' @param path_data Path to data
 #' @param mydir Output directory
-#' @param knitmydir output knit
 #' @return Report
 #' @export
 #' @importFrom utils choose.files head
 
-creating_report <- function(mydir = getwd(), knitmydir = getwd()){
+creating_report <- function(mydir = getwd()){
   path_list <- c(
-    #"CT_ARTIGOS"  = system.file("rmd", "report_artigos.Rmd", package = "dataedittecnology"),
-    #"CT_BOLSAPOS" = system.file("rmd", "report_bolsapos.Rmd", package = "dataedittecnology"),
-    #"CT_BNDES"    = system.file("rmd", "report_bndes.Rmd", package = "dataedittecnology"),
-    #"CT_DOUTORES" = system.file("rmd", "report_doutores.Rmd", package = "dataedittecnology"),
     "CT_"         = system.file("rmd", "report_ct.Rmd", package = "dataedittecnology")
   )
 
@@ -31,6 +26,6 @@ creating_report <- function(mydir = getwd(), knitmydir = getwd()){
 
   rmarkdown::render(input = path_list[prefix],
                     output_dir = mydir,
-                    knit_root_dir = knitmydir,
+                    knit_root_dir = mydir,
                     params = list(data = path_data))
 }
